c1fcfa3ae30f2305e1b935229a5c1f9eb55ffcd9
sladeberryman <slade12berryman@gmail.com>
2023-09-10 16:56:43 -0500

    Green Goblin Game

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,6 @@
 $( document ).ready(function() {
   var points = 0;
-  
-  $('#meme').click(
-    function() {
-      console.log("CLICK");
-      points += 2;
-      updatePoints();
-    });
+
   
   $('#meme').draggable({
     start:function() {
@@ -14,19 +8,41 @@ $( document ).ready(function() {
       console.log($('#meme').offset().left);
       
       if ($('#meme').offset().left < 200) {
-        points += 5;
+        message = "You're warm!";
       } else if (200 < $('#meme').offset().left < 600) {
-        points += 7;
-      } else {
-        points += 9;
+        message = "Look closer!";
       }
-      updatePoints();
+      updateMessage();
+    },
+
+    drag: function(event, ui) {
+      var memeRect = ui.helper[0].getBoundingClientRect();
+      var goblinRect = $('#goblin')[0].getBoundingClientRect();
+
+      if (isColliding(memeRect, goblinRect)) {
+        $('#message-display').text("You defeated the Goblin! Refresh to start over!");
+        $('#goblin').hide();
+      }
     }
+    
   });
   
-  function updatePoints() {
-    $('#points-display').text("Wow Points: " + points);
+  function updateMessage() {
+    if (!$('#goblin').is(':hidden')) {
+      $('#message-display').text(message);
+    }
   }
-  
-  
+
+  function isColliding(rect1, rect2) {
+    return (
+      rect1.left < rect2.right &&
+      rect1.right > rect2.left &&
+      rect1.top < rect2.bottom &&
+      rect1.bottom > rect2.top
+    );
+  }
+
+
 });
+
+
